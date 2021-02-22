0352e7793d1b403be266ae8e5a58bf93008addce
JASON ROUNDTREE <jasontree123@gmail.com>
2021-02-21 19:12:26 -0800

    Add get route for all photos and log response

--- server.js
+++ server.js
@@ -6,3 +6,19 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/photos', (req, res) => {
+    cloudinary
+        .search
+        .expression('resource_type:image')
+        .max_results(3)
+        .execute()
+        .then(result => {
+            console.log('photos: ', result)
+            res.json(result) 
+        })
+})
+
+const server = app.listen(process.env.PORT || 9000, () => {
+    console.log('Listening on port %d', server.address().port)
+})
