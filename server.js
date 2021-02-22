e5d1a5ad38e3b93ef9a7b32ac025082723ab8820
JASON ROUNDTREE <jasontree123@gmail.com>
2021-02-21 16:57:19 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const cloudinary = require('cloudinary').v2
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
